160de577caa6fd5b36cd7637aa7edfa82e35b14b
Ratikulislam <joyofficial80@gmail.com>
2023-11-30 10:14:17 +0600

    created-patch-method-and-deployed-to-vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.brfqf5n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,6 +42,19 @@ async function run() {
             app.get("/donors", async (req, res) => {
                   const result = await donorCollection.find().toArray();
                   res.send(result);
+            });
+
+            app.patch('/donors/admin/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const filter = { _id: new ObjectId(id) };
+                  const updatedDoc = {
+                        $set: {
+                              role: 'admin'
+                        }
+                  };
+                  const result = await donorCollection.updateOne(filter, updatedDoc);
+                  res.send(result);
+
             });
 
 
